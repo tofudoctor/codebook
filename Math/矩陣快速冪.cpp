5f6d90bb4b119d78b38d597ae290dbeeed950bd0
tofudoctor <87269360+tofudoctor@users.noreply.github.com>
2024-09-07 01:01:39 +0800

    矩陣快速冪

---
+++ Math/矩陣快速冪.cpp
@@ -0,0 +1,37 @@
+const int mod = 1000000007;
+
+struct Matrix {
+  int a[3][3];
+
+  Matrix() { memset(a, 0, sizeof a); }
+
+  Matrix operator*(const Matrix &b) const {
+    Matrix res;
+    for (int i = 1; i <= 2; ++i)
+      for (int j = 1; j <= 2; ++j)
+        for (int k = 1; k <= 2; ++k)
+          res.a[i][j] = (res.a[i][j] + a[i][k] * b.a[k][j]) % mod;
+    return res;
+  }
+} ans, base;
+
+void init() {
+  base.a[1][1] = base.a[1][2] = base.a[2][1] = 1;
+  ans.a[1][1] = ans.a[1][2] = 1;
+}
+
+void qpow(int b) {
+  while (b) {
+    if (b & 1) ans = ans * base;
+    base = base * base;
+    b >>= 1;
+  }
+}
+
+int main() {
+  int n = read();
+  if (n <= 2) return puts("1"), 0;
+  init();
+  qpow(n - 2);
+  println(ans.a[1][1] % mod);
+}
